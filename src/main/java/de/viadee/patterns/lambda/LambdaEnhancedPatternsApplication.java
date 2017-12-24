768adfb70d60c35cc7fb15a0fa7972abab094a79
Nockemann <Christian.Nockemann@viadee.de>
2017-12-24 14:25:59 +0100

    Spring starter project

---
+++ src/main/java/de/viadee/patterns/lambda/LambdaEnhancedPatternsApplication.java
@@ -0,0 +1,12 @@
+package de.viadee.patterns.lambda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LambdaEnhancedPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LambdaEnhancedPatternsApplication.class, args);
+	}
+}
